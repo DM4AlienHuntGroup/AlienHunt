d62fce313f59a8c035461989870bce0a0e83564a
TimothyBennett1 <timothybennett2323@gmail.com>
2016-10-31 17:02:15 -0500

    Changed all images to the correct ones and updated positions and scales

--- src/controllers/homeCtrl.js
+++ src/controllers/homeCtrl.js
@@ -19,11 +19,11 @@ angular.module( "app" )
 		renderer.autoResize = true;
 
 
-		var spaceship = new PIXI.Sprite.fromImage('./imgs/UFO.png');
-		var alien = new PIXI.Sprite.fromImage('./imgs/alien.png')
+		var spaceship = new PIXI.Sprite.fromImage('./imgs/UFO2.png');
+		var alien = new PIXI.Sprite.fromImage('./imgs/alien2.png')
 		var background = new PIXI.Sprite.fromImage('./imgs/Background.png');
-		var corn = new PIXI.Sprite.fromImage('./imgs/corn.png');
-		var scoreboard = new PIXI.Sprite.fromImage('./imgs/Scoreboard.png');
+		var corn = new PIXI.Sprite.fromImage('./imgs/grass.png');
+		var scoreboard = new PIXI.Sprite.fromImage('./imgs/scoreboard-3.png');
 
 		let hunted = false;
 
@@ -38,30 +38,31 @@ angular.module( "app" )
 
 
 
-		  alien.anchor.set = 0.1;
-		  alien.position.x = window.innerWidth / 2;
-		  alien.position.y = 100;
-		  alien.scale.set(0.3)
+		  alien.anchor.set = 0.5;
+		  alien.position.x = window.innerWidth / 6;
+		  alien.position.y = window.innerHeight / 2.155;
+		  alien.scale.x = 4.5;
+		  alien.scale.y = 6;
 
 		  spaceship.anchor.x = 0.5;
-			spaceship.anchor.y = 0.5;
+		  spaceship.anchor.y = 0.5;
 
-		  spaceship.scale.set(0.4);
+		  spaceship.scale.set(2.5);
 		  spaceship.position.x = Math.random() * renderer.width;
 		  spaceship.position.y = Math.random() * renderer.height;
 
-		  scoreboard.anchor.y = 0.9;
+		  scoreboard.anchor.y = 1;
 		  scoreboard.position.x = 0;
 		  scoreboard.position.y = window.innerHeight;
-		  scoreboard.scale.x = 1.215;
-		  scoreboard.scale.y = 0.65;
+		  scoreboard.scale.x = 5.15;
+		  scoreboard.scale.y = 2.8;
 		  // console.log(scoreboard);
 
-		  corn.anchor.y = 0.9;
-		  corn.position.y = window.innerHeight - 100;
+		  corn.anchor.y = 1;
+		  corn.position.y = window.innerHeight - 135;
 		  // console.log(scoreboard._texture._frame);
-		  corn.scale.x = 1.215;
-		  corn.scale.y = 0.8;
+		  corn.scale.x = 5.15;
+		  corn.scale.y = 3;
 		  // corn.postion.x =
 		  // corn.postion.x =
 		  // corn.scale.set
