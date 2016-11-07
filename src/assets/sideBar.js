542f77803278584b344c65ef87d72e74c756cacf
TimothyBennett1 <timothybennett2323@gmail.com>
2016-11-07 09:29:43 -0600

    added the file for the sidebar and added it to the css and index

---
+++ src/assets/sideBar.js
@@ -0,0 +1,7 @@
+function openBar() {
+  document.body.getElementById('sideBar').style.width = '250px';
+}
+
+function closeBar() {
+  document.body.getElementById('sideBar').style.width = '250px';
+}
