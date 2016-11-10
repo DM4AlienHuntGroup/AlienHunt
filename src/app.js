d0856417f3520ae80ba1259220074cbc746f42ab
SheaClose <SheaClose@gmail.com>
2016-11-09 23:59:16 -0600

    merge master

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import angular from "angular";
 import uiRouter from "angular-ui-router";
 import homeCtrl from "./controllers/homeCtrl.js";
 import homeService from "./services/homeService.js";
+import playService from "./Components/play.js"
 import splashPageDirective from "../public/directives/splashPageDirective.js";
 import splashTmpl from "../public/splash.html";
 import howler from "../howler.js";
@@ -10,6 +11,7 @@ import modal from "../src/assets/modal.js";
 
 angular.module( "app", [ "ui.router" ] )
 .service("homeService", homeService)
+.service("playService", playService)
 .directive("splashPage", splashPageDirective)
 .config( ( $stateProvider, $urlRouterProvider, $compileProvider ) => {
 	$compileProvider.debugInfoEnabled( true );
