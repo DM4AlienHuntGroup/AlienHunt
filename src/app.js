4951160995b68a73bd4f8d8625c87650413e25eb
SheaClose <SheaClose@gmail.com>
2017-05-30 22:02:43 -0500

    newe bundle

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ import angular from "angular";
 import uiRouter from "angular-ui-router";
 import homeCtrl from "./controllers/homeCtrl.js";
 import homeService from "./services/homeService.js";
-import playService from "./Components/play.js"
+import playService from "./Components/play.js";
 import splashPageDirective from "../public/directives/splashPageDirective.js";
 import splashTmpl from "../public/splash.html";
 import howler from "../howler.js";
 import modal from "../src/assets/modal.js";
 
-angular.module( "app", [ "ui.router" ] )
-.service("homeService", homeService)
-.service("playService", playService)
-.directive("splashPage", splashPageDirective)
-.config( ( $stateProvider, $urlRouterProvider, $compileProvider ) => {
-	$compileProvider.debugInfoEnabled( true );
-	$urlRouterProvider.otherwise( "/" );
-	$stateProvider
-	.state( "splashPage", {
-		url: "/"
-	, template: "<splash-page></splash-page>"
-	, controller: homeCtrl
-	} )
-} )
+angular
+  .module( "app", [ "ui.router" ] )
+  .service( "homeService", homeService )
+  .service( "playService", playService )
+  .directive( "splashPage", splashPageDirective )
+  .config( ( $stateProvider, $urlRouterProvider, $compileProvider ) => {
+    $compileProvider.debugInfoEnabled( true );
+    $urlRouterProvider.otherwise( "/" );
+    $stateProvider.state( "splashPage", {
+      url: "/"
+      , template: "<splash-page></splash-page>"
+      , controller: homeCtrl
+    } );
+  } );
